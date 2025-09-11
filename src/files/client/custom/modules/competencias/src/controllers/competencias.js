0afdfcc8382a8a90985bea4e3e36ccc1c3068279
Erksper <erksper@gmail.com>
2025-09-11 00:06:43 -0400

    pruebas

--- src/files/client/custom/modules/competencias/src/controllers/competencias.js
+++ src/files/client/custom/modules/competencias/src/controllers/competencias.js
@@ -6,13 +6,10 @@ define(['controllers/base'], function (BaseController) {
         },
         
         actionList: function () {
-            console.log('Redirigiendo desde actionList a CompetenciasApp');
-            this.getRouter().navigate('#CompetenciasApp', {trigger: true, replace: true});
-        },
-        
-        actionIndex: function () {
-            console.log('Redirigiendo desde actionIndex a CompetenciasApp');
-            this.getRouter().navigate('#CompetenciasApp', {trigger: true, replace: true});
+            console.log('Cargando aplicación Competencias directamente');
+            this.entire('CompetenciasIndex', {}, function (view) {
+                view.render();
+            });
         },
         
         defaultAction: 'list'
